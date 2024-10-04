6bbcd3c562de467e7c81bf63a46549b4b3492aac
HeavenlyOne <106679395+HeavenlyOne@users.noreply.github.com>
2024-10-04 14:40:40 +0300

    Tasks complited

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player', {
+    
+});
+
+function addingToLS (data) {
+   localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds) )
+}
+
+player.on('timeupdate', throttle(addingToLS, 1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
 
